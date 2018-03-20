1fdeee9b3bfcb1e5d7bda1cf2fe1cb4c9f487ae4
Phil Werner <philwerner@shaw.ca>
2018-03-20 04:04:20 +0000

    Finished adding comments

--- download_avatars.js
+++ download_avatars.js
@@ -16,11 +16,16 @@ function getRepoContributors(repoOwner, repoName, cb) {
 
   request(options, function(err, res, body) {
 
-    dataObj = JSON.parse(body);
-
+    dataObj = JSON.parse(body);  //Takes in the body of the get request, then
+                                // parses the data into JSON and sets dataObj equal to it
     cb(err, dataObj);
   });
 }
+
+/* This function takes in a url and filepath as parameters.  It then
+performs a get request on the url, and then pipes the data it receives
+to the given file path. */
+
 
 function downloadImageByURL(url, filePath) {
 
@@ -32,12 +37,24 @@ function downloadImageByURL(url, filePath) {
 
 console.log("Welcome to the GitHub Avatar Downloader");
 
+/* Here we check if the avatars directory doesn't exist yet,
+in which case we create it */
+
+if (!fs.existsSync('avatars')) {
+  fs.mkdirSync('avatars');
+}
+
 var args = process.argv.slice(2);
+
+/* Here we check to make sure there are two arguements before continuing. */
 
 if (args[0] && args[1]) {
 
   getRepoContributors(args[0], args[1], function(err, result) {
     console.log("Errors:", err);
+
+/* The for loop goes through each result and calls downloadImageByUrl
+  with each constructed url and output path  */
 
     for (var x = 0; x < result.length; x++) {
       downloadImageByURL(result[x]['avatar_url'], 'avatars/' + result[x]['login'] + '.jpg');
