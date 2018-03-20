262e57995ff094e289e4a889ad9fc4ad91d19a2c
Phil Werner <philwerner@shaw.ca>
2018-03-20 01:48:24 +0000

    Added secret token secretly

--- download_avatars.js
+++ download_avatars.js
@@ -1,12 +1,23 @@
 var request = require('request');
-
-//console.log("Welcome to the GitHub Avatar Downloader");
+var secret = require('./secrets.js');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  // ...
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request',
+      'Authorization': 'token ' + secret.GITHUB_TOKEN
+    }
+  };
+
+  request(options, function(err, res, body) {
+    cb(err, body);
+  });
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
   console.log("Result:", result);
 });
+
+console.log("Welcome to the GitHub Avatar Downloader");
